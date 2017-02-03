c72e732fdd9e8ad9139100f0ce77985a6388bd9c
iamyusuf6@aol.com <1998groudon>
2017-02-03 18:19:13 -0500

    Finished Furniture

--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,12 +9,16 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
+            //DONE
             double pCost = 69.43;
             double oCost = 40.4;
             double mCost = 493.32;
-            Console.WriteLine("Pease choose.  P for pine, O for Oak, or M for mahogany.");
-            string choice = Console.ReadLine();
-            if (choice == P) Console.WriteLine("Cost: " + pCost + "$");
+            Console.WriteLine("Please choose.  P for pine, O for Oak, or M for mahogany.");
+            string choice = Console.ReadLine().ToLower();
+            if (choice == "p") Console.WriteLine("Cost: " + pCost + "$ for Pine");
+            if (choice == "o") Console.WriteLine("Cost " + oCost + "$ for Oak");
+            if (choice == "m") Console.WriteLine("Cost " + mCost + "$ for Mahogany");
+            else Console.WriteLine("Cost: 0$");
             
         }
     }
